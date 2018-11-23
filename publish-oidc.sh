01d94303de176cb48f6a927d7df8505f2a161332
Marcus <marcus@hardt-it.de>
2018-11-23 12:16:57 +0100

    add helpful output

--- publish-oidc.sh
+++ publish-oidc.sh
@@ -9,7 +9,7 @@ set -e
 VERSION=$1
 
 BASE="/var/cache/pbuilder/"
-GITHUB="/home/buil/github"
+GITHUB="/home/build/github"
 OIDC_AGENT=oidc-agent_${VERSION}_amd64.deb
 PACKAGES="$OIDC_AGENT \
     oidc-agent_${VERSION}.dsc \
@@ -29,21 +29,19 @@ test -d $GITHUB && rm -f $GITHUB/*
 echo "Please be aware that this folder will be erased and recreated by
 scripts" > $GITHUB/README.txt
 
-for i in libsodium18_1.0.11-2_amd64.deb \
-         $OIDC_AGENT; \
-     do 
-         cp $BASE/result/xenial-amd64/$i $GITHUB/xenial-$i
-     done
+# copy to github
+cp $BASE/deps/xenial-amd64/oidc-agent/libsodium18_1.0.11-2_amd64.deb $GITHUB/xenial-libsodium18_1.0.11-2_amd64.deb
 
 for i in $DEB_DISTROS $UBU_DISTROS; do 
     cp $BASE/result/$i-amd64/$OIDC_AGENT $GITHUB/$i-$OIDC_AGENT
 done
+echo "github output done. See '$GITHUB'"
 
 # copy to repo 
 test -d $DEB_REPO || mkdir $DEB_REPO
 test -d $UBU_REPO || mkdir $UBU_REPO
 
-cp $BASE/result/xenial-amd64/libsodium18_1.0.11-2_amd64.deb $UBU_REPO/xenial || echo "error"
+cp $BASE/deps/xenial-amd64/oidc-agent/libsodium18_1.0.11-2_amd64.deb $UBU_REPO/xenial 
 
 for i in $DEB_DISTROS; do 
     test -d $DEB_REPO/$i || mkdir $DEB_REPO/$i
@@ -57,6 +55,7 @@ for i in $UBU_DISTROS; do
         cp $BASE/result/$i-amd64/$p $UBU_REPO/$i 2> /dev/null 
     done
 done
+echo "debian output done. To really update the repo, the package and release files still have to be created _and_signed_."
 
 #chown -R marcus.users $DEB_REPO $UBU_REPO
 #rsync -rlutopgxv --delete /var/cache/pbuilder/result/ root@cvs.fzk.de:/var/www/oidc-agent/
