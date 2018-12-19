3befebf8d97523a4d10385878caa2d36537a8c9f
Marcus <marcus@hardt-it.de>
2018-12-19 14:53:19 +0100

    log do files

--- cow-update-all.sh
+++ cow-update-all.sh
@@ -2,7 +2,13 @@
 #
 FILE=oidc-agent_2.0.0.dsc
 
-sudo                  HOME=$HOME DIST=stretch cowbuilder --update
-sudo                  HOME=$HOME DIST=buster  cowbuilder --update
-sudo DEPS=oidc-agent  HOME=$HOME DIST=xenial  cowbuilder --update
-sudo                  HOME=$HOME DIST=bionic  cowbuilder --update
+export DIST=stretch
+sudo                  HOME=$HOME  DIST=$DIST cowbuilder --update > ${DIST}_update.log
+export DIST=buster
+sudo                  HOME=$HOME  DIST=$DIST cowbuilder --update > ${DIST}_update.log
+export DIST=xenial  
+sudo DEPS=oidc-agent  HOME=$HOME  DIST=$DIST cowbuilder --update > ${DIST}_update.log
+export DIST=bionic  
+sudo                  HOME=$HOME  DIST=$DIST cowbuilder --update > ${DIST}_update.log
+
+ls *_update.log
