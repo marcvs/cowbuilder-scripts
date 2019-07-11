b96a265d37b90818585bebee46d1a76a8a68d9b0
Marcus <marcus@hardt-it.de>
2019-07-11 09:10:35 +0200

    local changes that are in use for quite some time now

--- cow-build-oidc-all.sh
+++ cow-build-oidc-all.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 #
-#FILE=oidc-agent_2.0.1.dsc
-#FILE_23="${1}_libsodium23"
-#FILE_18="${1}_libsodium18"
+DEB_SRC_LOG=oidc-agent-deb-src.log
+OIDC_AGENT_DIR="${HOME}/oidc-agent-deb/oidc-agent"
+
 [ -z $1 ] && {
     echo "The version of oidc-agent must be the only parameter. Ex: 2.0.3"
     exit 1
 }
 VERSION=$1
-OIDC_AGENT_DIR="${HOME}/oidc-agent-deb/oidc-agent"
-(cd $OIDC_AGENT_DIR && debuild -uc -us)
-FILE="oidc-agent_${VERSION}.dsc"
 
-sudo ls -l $FILE
+echo "Building oidc-agent version $VERSION using $OIDC_AGENT_DIR"
+echo "logs go to $DEB_SRC_LOG"
+
+cd $OIDC_AGENT_DIR 
+echo "Pulling changes"
+pushd ./ > /dev/null
+git pull
+debuild -uc -us > $DEB_SRC_LOG
+cd ..
+
+FILE="oidc-agent_${VERSION}.dsc"
+sudo ls -l $FILE || {
+    echo ".dsc file not found. I was expecting: $FILE"
+    exit 1
+}
 
 DIST=stretch
 (
@@ -41,3 +52,5 @@ echo "Building for $DIST..."
 sudo                  HOME=$HOME DIST=$DIST cowbuilder --build $FILE > buildlog-$DIST.log 2>&1
 echo "   $DIST: $?"
 )&
+
+popd > /dev/null
