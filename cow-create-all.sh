860ead45f5c7ebe8725d31b9adbedd5a12da5f0e
Marcus <marcus@hardt-it.de>
2020-12-17 14:32:29 +0100

    add focal; remove stretch

--- cow-create-all.sh
+++ cow-create-all.sh
@@ -14,11 +14,11 @@ sudo HOME=$HOME DIST=$DIST cowbuilder --create \
     --basepath /var/cache/pbuilder/${DIST}-amd64/base.cow/ \
     --distribution $DIST
 
-export DIST=stretch
-sudo mkdir -p $BASE/$DIST-amd64/aptcache
-sudo HOME=$HOME DIST=$DIST cowbuilder --create \
-    --basepath /var/cache/pbuilder/${DIST}-amd64/base.cow/ \
-    --distribution $DIST
+# export DIST=stretch
+# sudo mkdir -p $BASE/$DIST-amd64/aptcache
+# sudo HOME=$HOME DIST=$DIST cowbuilder --create \
+#     --basepath /var/cache/pbuilder/${DIST}-amd64/base.cow/ \
+#     --distribution $DIST
 
 # UBUNTU
 #key for bionic:
@@ -26,6 +26,14 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3B4FE6ACC
 # key for xenial: 
 wget http://archive.ubuntu.com/ubuntu/project/ubuntu-archive-keyring.gpg
 sudo mv ubuntu-archive-keyring.gpg /usr/share/keyrings/
+
+export DIST=focal
+sudo mkdir -p $BASE/$DIST-amd64/aptcache
+sudo HOME=$HOME DIST=$DIST cowbuilder --create \
+    --basepath /var/cache/pbuilder/${DIST}-amd64/base.cow/ \
+    --distribution $DIST \
+    --mirror http://archive.ubuntu.com/ubuntu \
+    --components "main universe"
 
 export DIST=bionic
 sudo mkdir -p $BASE/$DIST-amd64/aptcache
